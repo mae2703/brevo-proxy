4b16f0d36c1b4eb669ce89255bed7be378412f9e
mae2703 <30537924+mae2703@users.noreply.github.com>
2025-04-14 19:35:05 -0600

    Update app.js
    m

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.get('/brevo', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor recio corriendo en http://localhost:${port}`);
 });
